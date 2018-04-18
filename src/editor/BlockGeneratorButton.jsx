b5eaddaf006502032d88e53a6fb8285e81fc9365
Chia Yu Pai <fantasyatelier@gmail.com>
2018-04-19 02:46:26 +0800

    generate button will detect focus block and create block after it now

--- src/editor/BlockGeneratorButton.jsx
+++ src/editor/BlockGeneratorButton.jsx
@@ -1,12 +1,18 @@
 // @flow
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import type { EditorType } from '../type';
 import { EditorContext } from '../context';
 
 type Props = {
   type: EditorType,
   children: Node,
+};
+
+type State = {
+  focusedBlock: ?{
+    id: number,
+  },
 };
 
 const styles = {
@@ -18,22 +24,50 @@ const styles = {
   },
 };
 
-function BlockGeneratorButton({
-  type,
-  children,
-}: Props) {
-  return (
-    <EditorContext.Consumer>
-      {({ createBlock }) => (
-        <button
-          onClick={() => createBlock(type)}
-          style={styles.createBtn}
-          type="button">
-          {children}
-        </button>
-      )}
-    </EditorContext.Consumer>
-  );
+class BlockGeneratorButton extends PureComponent<Props, State> {
+  state = {
+    focusedBlock: null,
+  }
+
+  render() {
+    const {
+      type,
+      children,
+    } = this.props;
+
+    const {
+      focusedBlock,
+    } = this.state;
+
+    return (
+      <EditorContext.Consumer>
+        {({
+          createBlock,
+          blocks,
+        }) => (
+          <button
+            onMouseEnter={() => {
+              const activedBlock = blocks.find(b =>
+                b.input.current === document.activeElement);
+
+              if (activedBlock !== focusedBlock) {
+                this.setState({
+                  focusedBlock: activedBlock,
+                });
+              }
+            }}
+            onClick={() => createBlock(
+              type,
+              focusedBlock ? focusedBlock.id : null,
+            )}
+            style={styles.createBtn}
+            type="button">
+            {children}
+          </button>
+        )}
+      </EditorContext.Consumer>
+    );
+  }
 }
 
 export default BlockGeneratorButton;
