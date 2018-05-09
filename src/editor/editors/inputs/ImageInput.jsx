56b4b378d6922a6e04848eb590b7d9cb27de90c3
Chia Yu Pai <fantasyatelier@gmail.com>
2018-05-09 16:51:55 +0800

    auto resize images

--- src/editor/editors/inputs/ImageInput.jsx
+++ src/editor/editors/inputs/ImageInput.jsx
@@ -8,12 +8,15 @@ const styles = {
   wrapper: {
     width: '100%',
     position: 'relative',
-    height: '20vh',
+    minHeight: '20vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fafafa',
     padding: '0.25em 0.5em',
+  },
+  imageWrapper: {
+    position: 'relative',
   },
   input: {
     opacity: 0,
@@ -26,8 +29,25 @@ const styles = {
     zIndex: 10,
   },
   image: {
-    maxHeight: '100%',
-    maxWidth: '100%',
+
+  },
+  minSize: {
+    minWidth: '75%',
+    minHeight: '15vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputWrapper: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 10,
+  },
+  inputWrapperHovered: {
+    backgroundColor: 'rgba(0, 0, 0, 0.04)',
   },
 };
 
@@ -40,27 +60,121 @@ type Props = {
 };
 
 class ImageInput extends PureComponent<Props> {
+  constructor(props) {
+    super(props);
+
+    this.wrapper = React.createRef();
+  }
+
+  state = {
+    width: null,
+    height: null,
+    shownImage: null,
+    isHovered: false,
+    inputKey: Math.random(),
+  }
+
+  componentDidUpdate({ value: pastImage }) {
+    const {
+      value: newImage,
+    } = this.props;
+
+    if (newImage && pastImage !== newImage) {
+      this.updateImageSize(newImage);
+    }
+  }
+
+  updateImageSize(image) {
+    const { width } = this.wrapper.current.getBoundingClientRect();
+    const widthLimit = width * 0.9;
+    const heightLimit = window.innerHeight * 0.4;
+
+    const img = new Image();
+    img.onload = () => {
+      const ratio = img.width / img.height;
+
+      if (ratio >= 1) {
+        const targetWidth = Math.min(widthLimit, img.width);
+
+        this.setState({
+          shownImage: image,
+          width: targetWidth,
+          height: img.height / (img.width / targetWidth),
+        });
+      } else {
+        const targetHeight = Math.min(heightLimit, img.height);
+
+        this.setState({
+          shownImage: image,
+          width: img.width / (img.height / targetHeight),
+          height: targetHeight,
+        });
+      }
+    };
+    img.src = image;
+  }
+
   render() {
     const {
       onChange,
-      value,
       input,
     } = this.props;
 
+    const {
+      width,
+      height,
+      isHovered,
+      inputKey,
+      shownImage,
+    } = this.state;
+
+    const isImageLoaded = width && height && shownImage;
+
     return (
-      <div style={styles.wrapper}>
-        {value ? (
-          <img src={value} alt="" style={styles.image} />
+      <div style={styles.wrapper} ref={this.wrapper}>
+        {isImageLoaded ? (
+          <div style={styles.imageWrapper}>
+            <img src={shownImage} width={width} height={height} alt="" style={styles.image} />
+            <div
+              style={{
+                ...styles.inputWrapper,
+                ...(isHovered ? styles.inputWrapperHovered : {}),
+              }}
+              onMouseEnter={() => this.setState({ isHovered: true })}
+              onMouseLeave={() => this.setState({ isHovered: false })}>
+              <input
+                ref={input}
+                key={inputKey}
+                onChange={onChange}
+                style={styles.input}
+                accept="image/*"
+                type="file" />
+            </div>
+          </div>
         ) : (
-          <IconPhoto scale={1.5} />
+          <div
+            style={{
+              ...styles.imageWrapper,
+              ...styles.minSize,
+            }}>
+            <IconPhoto scale={1.5} />
+            <div
+              style={{
+                ...styles.inputWrapper,
+                ...(isHovered ? styles.inputWrapperHovered : {}),
+              }}
+              onMouseEnter={() => this.setState({ isHovered: true })}
+              onMouseLeave={() => this.setState({ isHovered: false })}>
+              <input
+                ref={input}
+                key={inputKey}
+                onChange={onChange}
+                style={styles.input}
+                accept="image/*"
+                type="file" />
+            </div>
+          </div>
         )}
-        <input
-          ref={input}
-          key={value || Math.random()}
-          onChange={onChange}
-          style={styles.input}
-          accept="image/*"
-          type="file" />
       </div>
     );
   }
